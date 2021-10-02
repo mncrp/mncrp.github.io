dc074ee43184f64c6b2cd51d33d45ac736812d45
Sorakime <69241694+Sorakime@users.noreply.github.com>
2021-10-02 21:17:27 +0900

    Update preload.js

--- mncr/script/preload.js
+++ mncr/script/preload.js
@@ -6,7 +6,7 @@ window.onload=()=>{
     <a href="/mncr/project/">Projects</a>
   </p>`
   document.getElementsByTagName('body')[0].innerHTML=`${document.getElementsByTagName('body')[0].innerHTML}
-  <small>Copyright 2021 Sorakime. Licensed by <a href="/mncr/license?v=1.1.1">monochrome. License.</a></small>`;
+  <small>Copyright &copy; 2021 Sorakime. Licensed by <a href="/mncr/license?v=1.1.1">monochrome. License.</a></small>`;
   document.getElementsByTagName('span')[0].addEventListener('click',()=>{
     location.href='/mncr/';
   })
